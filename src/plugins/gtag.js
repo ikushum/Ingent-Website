383b29da1477463b4401724770c6689b30419892
Ishan Subedi <ikushum@gmail.com>
2025-06-01 13:02:17 +0545

    track google analytics only on production

--- src/plugins/gtag.js
+++ src/plugins/gtag.js
@@ -1,5 +1,7 @@
-window.dataLayer = window.dataLayer || [];
-function gtag (){dataLayer.push(arguments);}
-gtag('js', new Date());
+if (import.meta.env.MODE === 'production') {
+  window.dataLayer = window.dataLayer || [];
+  function gtag (){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-gtag('config', import.meta.env.VITE_GOOGLE_ANALYTICS_KEY);
+  gtag('config', import.meta.env.VITE_GOOGLE_ANALYTICS_KEY);
+}
